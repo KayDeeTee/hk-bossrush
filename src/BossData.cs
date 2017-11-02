e5e177b9560dad900204cd59a14c31b3cb2a658f
KayDeeTee <kristian._thorpe@hotmail.co.uk>
2017-11-02 14:04:15 +0000

    Initial Commit

---
+++ src/BossData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace BossRush
+{
+    public class BossData
+    {
+        public BossData(string n, string scene, Vector2 pos, string[] items, ItemPos p, string var)
+        {
+            name = n;
+            sceneName = scene;
+            scenePos = pos;
+            drops = items;
+            killedVar = var;
+            itemPositions = p;
+        }
+
+        //Debug information
+        public string name;
+        //Scene Data
+        public string sceneName; public Vector2 scenePos;
+        //Item Data
+        public string[] drops;public ItemPos itemPositions;
+        //PlayerData variable to check when to progress
+        public string killedVar;        
+    }
+}
