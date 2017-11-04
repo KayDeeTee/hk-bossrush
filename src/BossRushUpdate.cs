0ee87a26e687664911aa2658cce43e19bbb463db
KayDeeTee <kristian._thorpe@hotmail.co.uk>
2017-11-04 21:33:16 +0000

    dev-branch

--- src/BossRushUpdate.cs
+++ src/BossRushUpdate.cs
@@ -13,15 +13,10 @@ namespace BossRush
         private bool spawnedItems = false;
         Rect itemRect = new Rect(new Vector2(5, (int)Screen.height / 2), new Vector2(500, 500));
 
-        public string getItemName(int x)
-        {
-            return BossRush.boolToString(BossRush.bossData[BossRush.currentBoss].drops[x]);
-        }
-
         public void OnGUI()
         {
             if (BossRush.gm.gameState == GameState.PLAYING && spawnedItems)
-                GUI.Label(itemRect, String.Format("L : {0}\nC : {1}\nR : {2}", getItemName(1), getItemName(0), getItemName(2)) );
+                GUI.Label(itemRect, String.Format("L : {0}\nC : {1}\nR : {2}", BossInfo.itemName(1), BossInfo.itemName(0), BossInfo.itemName(2)));
         }
 
         public void Update()
@@ -30,73 +25,34 @@ namespace BossRush
             {   
                 if (BossRush.gm.sceneName == "Tutorial_01")
                 {
-                    BossRush.Teleport(BossRush.getScene(), BossRush.getPos());
+                    BossInfo.toNextBoss(false);
                 }
-                if (!BossRush.bossKilled() && BossRush.gm.sceneName != BossRush.getScene())
+                if (!BossInfo.isBossDead() && BossRush.gm.sceneName != BossInfo.getBossScene())
                 {
-                    BossRush.Teleport(BossRush.getScene(), BossRush.getPos());
+                    BossInfo.toNextBoss(false);
                 }
-                if (BossRush.bossKilled() && BossRush.gm.sceneName != "Dream_01_False_Knight" && BossRush.gm.sceneName != "Dream_02_Mage_Lord" && BossRush.gm.sceneName != "Dream_03_Infected_Knight" && BossRush.gm.sceneName != "Grimm_Nightmare")
+                if (BossInfo.isBossDead() && BossRush.gm.sceneName != "Dream_01_False_Knight" && BossRush.gm.sceneName != "Dream_02_Mage_Lord" && BossRush.gm.sceneName != "Dream_03_Infected_Knight" && BossRush.gm.sceneName != "Grimm_Nightmare")
                 {
                     if (!spawnedItems)
                     {
-                        Vector2 pos;
-                        GameObject s1 = Instantiate(BossRush.shiny);
-                        pos = BossRush.getItemPos(2);
-                        if (pos.x < -900)
-                            s1.transform.position = new Vector2(BossRush.hc.transform.position.x, BossRush.hc.transform.position.y);
-                        else
-                            s1.transform.position = pos;
-                        PlayMakerFSM shiny1FSM = FSMUtility.LocateFSM(s1, "Shiny Control");
-                        shiny1FSM.FsmVariables.GetFsmString("PD Bool Name").Value = "gotCharm_1";
-                        shiny1FSM.FsmVariables.GetFsmBool("Charm").Value = true;
-                        shiny1FSM.FsmVariables.GetFsmInt("Charm ID").Value = 1;
-
-                        FsmUtil.ChangeTransition(shiny1FSM, "PD Bool?", "COLLECTED", "Fling?");
-
-                        GameObject s2 = Instantiate(BossRush.shiny);
-                        pos = BossRush.getItemPos(1);
-                        if (pos.x < -900)
-                            s2.transform.position = new Vector2(BossRush.hc.transform.position.x-2.5f, BossRush.hc.transform.position.y);
-                        else
-                            s2.transform.position = pos;
-                        PlayMakerFSM shiny2FSM = FSMUtility.LocateFSM(s2, "Shiny Control");
-                        shiny2FSM.FsmVariables.GetFsmString("PD Bool Name").Value = "gotCharm_2";
-                        shiny2FSM.FsmVariables.GetFsmBool("Charm").Value = true;
-                        shiny2FSM.FsmVariables.GetFsmInt("Charm ID").Value = 2;
-
-                        FsmUtil.ChangeTransition(shiny2FSM, "PD Bool?", "COLLECTED", "Fling?");
-
-                        GameObject s3 = Instantiate(BossRush.shiny);
-                        pos = BossRush.getItemPos(3);
-                        if (pos.x < -900)
-                            s3.transform.position = new Vector2(BossRush.hc.transform.position.x + 2.5f, BossRush.hc.transform.position.y);
-                        else
-                            s3.transform.position = pos;
-                        PlayMakerFSM shiny3FSM = FSMUtility.LocateFSM(s3, "Shiny Control");
-                        shiny3FSM.FsmVariables.GetFsmString("PD Bool Name").Value = "gotCharm_3";
-                        shiny3FSM.FsmVariables.GetFsmBool("Charm").Value = true;
-                        shiny3FSM.FsmVariables.GetFsmInt("Charm ID").Value = 3;
-
-                        FsmUtil.ChangeTransition(shiny3FSM, "PD Bool?", "COLLECTED", "Fling?");
+                        BossInfo.SpawnAll();
 
                         spawnedItems = true;
 
                     }
                     else
                     {
-                        if (PlayerData.instance.gotCharm_1)
-                            BossRush.onPickup("gotCharm_1", true);
-                        if (PlayerData.instance.gotCharm_2)
-                            BossRush.onPickup("gotCharm_2", true);
-                        if (PlayerData.instance.gotCharm_3)
-                            BossRush.onPickup("gotCharm_3", true);
+                        if (PlayerData.instance.trinket1 > 0)
+                            BossInfo.pickupItem(0);
+                        if (PlayerData.instance.trinket2 > 0)
+                            BossInfo.pickupItem(1);
+                        if (PlayerData.instance.trinket3 > 0)
+                            BossInfo.pickupItem(2);
 
                         if (BossRush.pickups > (BossRush.flawless ? 1 : 0))
                         {
-                            BossRush.currentBoss++;
                             spawnedItems = false;
-                            BossRush.Teleport(BossRush.getScene(), BossRush.getPos());
+                            BossInfo.toNextBoss(true);
                         }
                     }
                 }
---
+++ src/ItemInfo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BossRush
+{
+    public class ItemInfo
+    {
+        public static List<ItemInfo> itemInfo;
+
+        public string internalName,sheetName,varName;
+        public string[] keyNames;
+        public int stepAmount;
+        public static Dictionary<string, int> upgrades;
+
+        public ItemInfo(string iN, string sN, string vN, int step, params string[] kN)
+        {
+            internalName = iN;
+            sheetName = sN;
+            keyNames = kN;
+            varName = vN;
+            stepAmount = step;
+        }
+
+        public static ItemInfo getRandomItem()
+        {
+            int r = UnityEngine.Random.RandomRange(0, ItemInfo.itemInfo.Count());
+            ItemInfo ret = ItemInfo.itemInfo.ElementAt(r);
+            ItemInfo.itemInfo.RemoveAt(r);
+            return ret;
+        }
+
+        public static void createItemInfo()
+        {
+            upgrades = new Dictionary<string, int>();
+
+            itemInfo = new List<ItemInfo>();
+            //ItemInfo(string iN, string sN, string kN, string vN, int step)
+            for (int i = 1; i < 40; i++)
+            {
+                if (i != 36)
+                    itemInfo.Add( new ItemInfo("CHARM_"+i.ToString(), "UI", "equippedCharm_"+i, 0, "CHARM_NAME_"+i ) );
+            }
+
+            itemInfo.Add(new ItemInfo("MothwingCloak", "UI", "hasDash", 0, "INV_NAME_DASH", "INV_NAME_SHADOWDASH")); itemInfo.Add(new ItemInfo("MothwingCloak", "UI", "hasDash", 0, "INV_NAME_DASH", "INV_NAME_SHADOWDASH"));
+            itemInfo.Add(new ItemInfo("MonarchWings", "UI", "hasDoubleJump", 0, "INV_NAME_DOUBLEJUMP")); itemInfo.Add(new ItemInfo("MantisClaw", "UI", "hasWallJump", 0, "INV_NAME_WALLJUMP"));
+            itemInfo.Add(new ItemInfo("Fireball", "UI", "fireballLevel", 1, "INV_NAME_SPELL_FIREBALL1", "INV_NAME_SPELL_FIREBALL2")); itemInfo.Add(new ItemInfo("Fireball", "UI", "fireballLevel", 1, "INV_NAME_SPELL_FIREBALL1", "INV_NAME_SPELL_FIREBALL2"));
+            itemInfo.Add(new ItemInfo("Dive", "UI", "quakeLevel", 1, "INV_NAME_SPELL_QUAKE1", "INV_NAME_SPELL_QUAKE2")); itemInfo.Add(new ItemInfo("Dive", "UI", "quakeLevel", 1, "INV_NAME_SPELL_QUAKE1", "INV_NAME_SPELL_QUAKE2"));
+            itemInfo.Add(new ItemInfo("Scream", "UI", "screamLevel", 1, "INV_NAME_SPELL_SCREAM1", "INV_NAME_SPELL_SCREAM2")); itemInfo.Add(new ItemInfo("Scream", "UI", "screamLevel", 1, "INV_NAME_SPELL_SCREAM1", "INV_NAME_SPELL_SCREAM2"));
+            itemInfo.Add(new ItemInfo("nailLevel", "UI", "nailDamage", 4, "INV_NAME_NAIL2", "INV_NAME_NAIL3", "INV_NAME_NAIL4", "INV_NAME_NAIL5"));
+            itemInfo.Add(new ItemInfo("nailLevel", "UI", "nailDamage", 4, "INV_NAME_NAIL2", "INV_NAME_NAIL3", "INV_NAME_NAIL4", "INV_NAME_NAIL5"));
+            itemInfo.Add(new ItemInfo("nailLevel", "UI", "nailDamage", 4, "INV_NAME_NAIL2", "INV_NAME_NAIL3", "INV_NAME_NAIL4", "INV_NAME_NAIL5"));
+            itemInfo.Add(new ItemInfo("nailLevel", "UI", "nailDamage", 4, "INV_NAME_NAIL2", "INV_NAME_NAIL3", "INV_NAME_NAIL4", "INV_NAME_NAIL5"));
+            itemInfo.Add(new ItemInfo("hpLevel", "RAW", "maxHealth", 1, "HP")); itemInfo.Add(new ItemInfo("hpLevel", "RAW", "maxHealth", 1, "HP"));
+            itemInfo.Add(new ItemInfo("hpLevel", "RAW", "maxHealth", 1, "HP")); itemInfo.Add(new ItemInfo("hpLevel", "RAW", "maxHealth", 1, "HP"));
+            itemInfo.Add(new ItemInfo("mpLevel", "RAW", "maxMp", 33, "MP"));
+            itemInfo.Add(new ItemInfo("mpLevel", "RAW", "maxMp", 33, "MP"));
+            itemInfo.Add(new ItemInfo("mpLevel", "RAW", "maxMp", 33, "MP"));
+            itemInfo.Add(new ItemInfo("dashSlash", "UI", "hasUpwardSlash", 0, "INV_NAME_ART_UPPER"));
+            itemInfo.Add(new ItemInfo("cyclone", "UI", "hasCyclone", 0, "INV_NAME_ART_CYCLONE"));
+            itemInfo.Add(new ItemInfo("greatSlash", "UI", "hasDashSlash", 0, "INV_NAME_ART_DASH"));
+            itemInfo.Add(new ItemInfo("grimmChild", "UI", "grimmChildLevel", 1, "CHARM_NAME_40"));
+            itemInfo.Add(new ItemInfo("grimmChild", "UI", "grimmChildLevel", 1, "CHARM_NAME_40"));
+            itemInfo.Add(new ItemInfo("grimmChild", "UI", "grimmChildLevel", 1, "CHARM_NAME_40"));
+            itemInfo.Add(new ItemInfo("grimmChild", "UI", "grimmChildLevel", 1, "CHARM_NAME_40"));
+        }
+
+    }
+}
